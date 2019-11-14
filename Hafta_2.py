a4f4d9725da2a5e80b3362e184741e1e84a119fa
mrkanet <mrkaan888@yandex.com>
2019-11-14 08:45:41 +0300

    new commit

--- Hafta_2.py
+++ Hafta_2.py
@@ -1,39 +1,91 @@
-import os
 import numpy as np
-os.getcwd()
 import matplotlib.pyplot as plt
 
-def minimizePic(pic):
-	m,n,p = pic.shape
-	pic2 = np.zeros((int(m/2),int(n/2),3), dtype=int)
-	new_m, new_n = int(m/2), int(n/2)
 
-	for m in range(new_m):
-		for n in range(new_n):
-			s0 = (pic[m,n,0] + pic[m,n,1] + pic[m,n,2])/3
-			s1 = (pic[m,n+1,0] + pic[m,n+1,1] + pic[m,n+1,2])/3
-			s2 = (pic[m+1,n,0] + pic[m+1,n,1] + pic[m+1,n,2])/3
-			s3 = (pic[m+1,n+1,0] + pic[m+1,n+1,1] + pic[m+1,n+1,2])/3
-			s = (s0+s1+s2+s3)/4
-			pic2[m,n] = s
 
-	return pic2 
+def my_function_1(my_list=[9,3,5,6,2,5,6]):
+    for i in range(len(my_list)):
+        my_list[i]=my_list[i]+1
+    print(my_list)
+my_function_1()
 
-plt.imshow(minimizePic(plt.imread("manzara.jpg") ))
-plt.show
+def my_function_2(my_array=np.array(list([9,3,5,6,2,5,6]))):
+    return my_array+10
+my_function_2()
 
-def minimizeImg(img):
-	m,n,p = img.shape
-	new_m, new_n = int(m/2), int(n/2)
-	new_img = np.zeros((new_m,new_n,3), dtype = int)
+im_1=plt.imread('istanbul.jpg')
+plt.imshow(im_1)
+plt.show()
 
-	for m in range(0,img.shape[0],2):
-		for n in range(0,img.shape[1],2):
-			s0 = (pic[m,n,0] + pic[m,n,1] + pic[m,n,2])/3
-			s1 = (pic[m,n+1,0] + pic[m,n+1,1] + pic[m,n+1,2])/3
-			s2 = (pic[m+1,n,0] + pic[m+1,n,1] + pic[m+1,n,2])/3
-			s3 = (pic[m+1,n+1,0] + pic[m+1,n+1,1] + pic[m+1,n+1,2])/3
-			s = (s0+s1+s2+s3)/4
-			new_img[m,n] = int(s)
-	return new_img
+m,n,p=im_1.shape
+im_2=np.zeros((m,n,3),dtype=int)
+m,n,im_2.shape
+for m in range(im_1.shape[0]):
+    for n in range(im_1.shape[1]):
+        im_2[m,n,0]=im_1[m,n,0]-25   #R bantını 25 eksiltiyor 
+        im_2[m,n,1]=im_1[m,n,1]
+        im_2[m,n,2]=im_1[m,n,2]
+plt.imshow(im_2)
+plt.show()
+
+m,n,p=im_1.shape
+im_2=np.zeros((m,n,3),dtype=int)
+m,n,im_2.shape
+for m in range(im_1.shape[0]):
+    for n in range(im_1.shape[1]):
+        im_2[m,n,0]=im_1[m,n,0]-25   #R bantını 25 eksiltiyor 
+        im_2[m,n,1]=im_1[m,n,1]
+        im_2[m,n,2]=im_1[m,n,2]
+plt.imshow(im_2)
+plt.show()
+
+def my_function_3(im_100,s):
+    im_1=im_100
+    m,n,p=im_1.shape
+    im_2=np.zeros((m,n,3),dtype=int)
+    m,n,im_2.shape
+    for m in range(im_1.shape[0]):
+        for n in range(im_1.shape[1]):
+            im_2[m,n,0]=im_1[m,n,0]-s   #R bantını 25 eksiltiyor 
+            im_2[m,n,1]=im_1[m,n,1]
+            im_2[m,n,2]=im_1[m,n,2]
+    return im_100
+
+def my_function_4(im_500):
+    m,n,p=im_500.shape
+    new_m=int(m/2)
+    new_n=int(n/2)
+    im_600=np.zeros((new_m,new_n),dtype=int)
+
+    for m in range(new_m):
+        for n in range(new_n):
+            s0=(im_500[m*2,n*2,0]+im_500[m*2,n*2,1]+im_500[m*2,n*2,2])/3 #bulunulan piksel
+            im_600[m,n]=int(s0)
+    return im_600
+
+im_6=my_function_4(im_1)
+plt.imshow(im_6,cmap='gray')
+plt.show()
+plt.imsave('istanbulGray.jpg',im_6,cmap='gray')
+
+def my_scale(im_7):
+    m,n,p=im_7.shape
+    new_m=int(m/2)
+    new_n=int(n/2)
+    im_8=np.zeros((new_m,new_n,3),dtype=int)
+    for m in range(new_m):
+        for n in range(new_n):
+            im_8[m,n,0]=int(im_7[m*2,n*2,0])
+            im_8[m,n,1]=int(im_7[m*2,n*2,1])
+            im_8[m,n,2]=int(im_7[m*2,n*2,2])
+    return im_8
+
+im_10=my_scale(im_1)
+plt.imshow(im_10)
+
+
+
+im_100=my_scale(im_10)
+plt.imshow(im_100)
+
 
