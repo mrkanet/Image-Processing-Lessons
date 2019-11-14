fb5c3b6cb0c2b658477c0075bd439cd6b9f562fe
mrkanet <mrkaan888@yandex.com>
2019-11-14 08:57:04 +0300

    new commit

--- Hafta_3.py
+++ Hafta_3.py
@@ -1,38 +1,63 @@
-#bu derste listeler ile işlemler yapıldı
-import os
-import numpy as np
-os.getcwd()
 import matplotlib.pyplot as plt
+import numpy as np
 
-def myfunc2 (myArray = np.array(1,2,3,4,5,6)):
-	return(myArray+10)
+my_list_1=[2,4,3,4,5,6,3,3,2,1]
+def my_function_1(my_list_1=[2,4,3,4,5,6,3,3,2,1]):
+    t=0
+    s=0
+    for i in my_list_1:
+        s=s+1
+        t=t+i
+    mean_1=t/s
+
+    t=0
+    s=0
+    for i in my_list_1:
+        s=s+1
+        t=t+(i-mean_1)*(i-mean_1)
+    var_1=t/(s-1)
+
+    return mean_1,var_1
+my_function_1()
+
+my_histogram={}
+for i in my_list_1:
+    if i in my_histogram.keys():
+        my_histogram[i]=my_histogram[i]+1
+    else:
+        my_histogram[i]=1
+my_histogram
 
 
-img = plt.imred("manzara") 
-#plt.imshow(img)
-#plt.show()
 
-print(img.ndim, img.shape)
-
-#m,n,p = img.shape
-#img2 = np.zeros((m,n,3), dtype = int)
-#for j in range(img.shape[0]):
-#	for i in range(img.shape[1]):
-#		img2[m,n,0] = img[j,i,0] + 25
+for i in my_histogram.keys():
+    print(i,my_histogram[i])
 
 
-def minimizePic(pic):
-	m,n,p = pic.shape
-	pic2 = np.zeros((m/2,n/2), dtype=int)
-	new_m, new_n = int(m/2), int(n/2)
-	
-	for m in range(new_m):
-		for n in range(new_n):
-			s0 = (pic[m,n,0] + pic[m,n,1] + pic[m,n,2])/3
-			s1 = (pic[m,n+1,0] + pic[m,n+1,1] + pic[m,n+1,2])/3
-			s2 = (pic[m+1,n,0] + pic[m+1,n,1] + pic[m+1,n,2])/3
-			s3 = (pic[m+1,n+1,0] + pic[m+1,n+1,1] + pic[m+1,n+1,2])/3
-			s = (s0+s1+s2+s3)/4
-			pic2[m,n] = s
-			
-	return pic2 
+image_1=plt.imread('truva.jpg')
+
+
+
+def my_function_2(image_1=plt.imread('truva.jpg')):
+    m,n,p=image_1.shape
+    my_histogram={}
+    for i in range(m):
+        for j in range(n):
+            if image_1[i,j,0] in my_histogram.keys():
+                my_histogram[image_1[i,j,0]]=my_histogram[image_1[i,j,0]]+1
+            else:
+                my_histogram[image_1[i,j,0]]=1
+    return my_histogram
+
+
+def my_function_3(my_histogram=my_function_2()):
+    x=[]
+    y=[]
+    for key in  my_histogram.keys():
+        x.append(key)
+        y.append(my_histogram[key])
+    return x,y
+
+x,y=my_function_3()
+plt.bar(x,y)
+plt.show()
